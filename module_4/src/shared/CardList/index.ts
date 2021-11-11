6f8f7dd896e831d033f98458b9c601fd71c0f281
Dimonmir <Diimonmir@mail.ru>
2021-11-12 00:26:20 +0300

    remark git

--- module_4/src/shared/CardList/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '../../../../module_5/src/shared/CardList/CardList';
-export {preventDefault} from "../../utils/react/PreventDefault";
--- module_4/src/shared/Header/ThreadTitle/ThreadTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../../../../../module_5/src/shared/Header/ThreadTitle/threadtitle.css';
-
-export function ThreadTitle() {
-  return (
-      <h1 className={styles.threadTitle}>Header</h1>
-  );
-}
